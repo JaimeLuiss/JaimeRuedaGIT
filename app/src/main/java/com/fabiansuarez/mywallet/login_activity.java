c86462fde1a4bd22bd92afa8ffff20b7dd6d7536
Jaime Rueda <jrueda793@unab.edu.co>
2023-10-13 09:36:03 -0500

    Login

--- app/src/main/java/com/fabiansuarez/mywallet/login_activity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/login_activity.java
@@ -2,7 +2,9 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login_activity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void buttonRegistrar(View view) {
+        Intent myIntent = new Intent(login_activity.this, register_activity.class);
+        startActivity(myIntent);
+        finish();
     }
 }
--- app/src/main/java/com/fabiansuarez/mywallet/register_activity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/register_activity.java
@@ -2,7 +2,9 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class register_activity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class register_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void buttonRegresar(View view) {
+        Intent myIntent = new Intent(register_activity.this, login_activity.class);
+        startActivity(myIntent);
+        finish();
     }
 }
